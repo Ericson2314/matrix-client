87bad0d215934742840eed17d1fbdadc47a9bbcd
Ken Micklas <kmicklas@gmail.com>
2018-11-09 19:47:04 -0500

    Add semantic-reflex override

--- default.nix
+++ default.nix
@@ -2,9 +2,16 @@
 , iosSdkVersion ? "10.2"
 }:
 with import ./.obelisk/impl { inherit system iosSdkVersion; };
-project ./. ({ ... }: {
+project ./. ({ pkgs, hackGet, ... }: {
   android.applicationId = "systems.obsidian.obelisk.examples.minimal";
   android.displayName = "Obelisk Minimal Example";
   ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
   ios.bundleName = "Obelisk Minimal Example";
+
+  packages = {
+    semantic-reflex = (hackGet ./dep/semantic-reflex) + /semantic-reflex;
+  };
+  overrides = self: super: {
+    semantic-reflex = pkgs.haskell.lib.dontHaddock super.semantic-reflex;
+  };
 })
