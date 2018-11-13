927ceb42917ba7dbdaa3e8612b61c16fc5f4f8bd
Ken Micklas <kmicklas@gmail.com>
2018-11-12 22:09:58 -0500

    Add more beam orphans for newtypes

--- common/src/Database/Beam/Matrix/Orphans.hs
+++ common/src/Database/Beam/Matrix/Orphans.hs
@@ -2,12 +2,21 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Database.Beam.Matrix.Orphans where
 
+import Data.Text
+import Database.Beam
+import Database.Beam.Backend
 import Database.Beam.Migrate
 
 import Matrix.Client.Types
 
+deriving newtype instance (HasSqlValueSyntax be Text) => HasSqlValueSyntax be DeviceId
+deriving newtype instance (BeamSqlBackend be, HasSqlEqualityCheck be Text) => HasSqlEqualityCheck be DeviceId
+deriving newtype instance (BeamBackend be, FromBackendRow be Text) => FromBackendRow be DeviceId
 deriving newtype instance BeamMigrateSqlBackend be => HasDefaultSqlDataTypeConstraints be DeviceId
 deriving newtype instance BeamMigrateSqlBackend be => HasDefaultSqlDataType be DeviceId
 
+deriving newtype instance (HasSqlValueSyntax be Text) => HasSqlValueSyntax be UserId
+deriving newtype instance (BeamSqlBackend be, HasSqlEqualityCheck be Text) => HasSqlEqualityCheck be UserId
+deriving newtype instance (BeamBackend be, FromBackendRow be Text) => FromBackendRow be UserId
 deriving newtype instance BeamMigrateSqlBackend be => HasDefaultSqlDataTypeConstraints be UserId
 deriving newtype instance BeamMigrateSqlBackend be => HasDefaultSqlDataType be UserId
