ba9e06547634a546b507f36ebb8764e65119ac80
Ken Micklas <kmicklas@gmail.com>
2018-11-12 19:09:54 -0500

    Abstract JSON XHR requests

--- frontend/src/Frontend/Request/Local.hs
+++ frontend/src/Frontend/Request/Local.hs
@@ -13,6 +13,7 @@ import Data.Aeson
 import Data.ByteString.Lazy (toStrict)
 import Data.Coerce
 import Data.Constraint
+import Data.Text (Text)
 import Data.Text.Encoding
 import Language.Javascript.JSaddle.Types
 import Obelisk.Route.Frontend
@@ -23,6 +24,14 @@ import Reflex.Host.Class
 import Matrix.Client.Types
 
 import Frontend.Request
+
+data XhrResponseParse response error
+  = XhrResponseParse_XhrException XhrException
+  | XhrResponseParse_Success response
+  | XhrResponseParse_Failure error
+  | XhrResponseParse_NoParseSuccess
+  | XhrResponseParse_NoParseFailure
+  deriving (Eq, Ord, Show)
 
 data LocalFrontendRequestContext t = LocalFrontendRequestContext
   {
@@ -91,23 +100,35 @@ handleLocalFrontendRequest
   -> Performable m ()
 handleLocalFrontendRequest k _ = \case
   FrontendRequest_Login hs u pw -> do
-    let handleResponse = \case
-          Left e -> do
-            print e
-            k ()
-          Right r -> do
-            print $ decodeXhrResponse @LoginResponse r
-            k ()
     let loginRequest = LoginRequest
           (UserIdentifier_User u)
           (Login_Password pw)
           Nothing
           Nothing
     let url = hs <> "/_matrix/client/r0/login"
-    let body = decodeUtf8 $ toStrict $ Data.Aeson.encode loginRequest
+    performXhrCallbackWithErrorPrerenderJSON @js @t @m "POST" url loginRequest $
+      \(r :: XhrResponseParse Data.Aeson.Value LoginResponse) -> do
+        print r
+        k ()
 
-    performRequestCallbackWithErrorPrerender @js @t @m handleResponse $
-      XhrRequest "POST" url $ def & xhrRequestConfig_sendData .~ body
+performXhrCallbackWithErrorPrerenderJSON
+  :: forall js t m request response error
+  .  (PerformEvent t m, Prerender js m, ToJSON request, FromJSON response, FromJSON error)
+  => Text
+  -> Text
+  -> request
+  -> (XhrResponseParse response error -> IO ())
+  -> Performable m ()
+performXhrCallbackWithErrorPrerenderJSON method url request k =
+  performRequestCallbackWithErrorPrerender @js @t @m k' $
+    XhrRequest method url $ def & xhrRequestConfig_sendData .~ body
+ where
+  body = decodeUtf8 $ toStrict $ Data.Aeson.encode request
+  k' = \case
+    Left e -> k $ XhrResponseParse_XhrException e
+    Right r -> if _xhrResponse_status r >= 200 && _xhrResponse_status r < 400
+      then k $ maybe XhrResponseParse_NoParseSuccess XhrResponseParse_Success $ decodeXhrResponse r
+      else k $ maybe XhrResponseParse_NoParseFailure XhrResponseParse_Failure $ decodeXhrResponse r
 
 performRequestCallbackWithError
   :: forall m a. (MonadJSM m, HasJSContext m, IsXhrPayload a)
