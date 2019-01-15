032f3f6fd3d28732a883edee4e15cc57112b80ec
Ken Micklas <kmicklas@gmail.com>
2019-01-15 16:33:40 -0500

    Add query widgets

--- frontend/src/Frontend/Query.hs
+++ frontend/src/Frontend/Query.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Frontend.Query where
 
+import           Control.Monad
 import           Data.Constraint.Extras.TH
 import           Data.Functor.Identity
 import           Data.GADT.Compare.TH
+import qualified Data.Map.Monoidal as MM
 import           Data.Semigroup
 import           Data.Set
 import           Data.Vessel
@@ -26,3 +28,24 @@ type FrontendV = Vessel V
 
 type FrontendQuery = FrontendV (Const SelectedCount)
 type FrontendQueryResult = FrontendV Identity
+
+queryLogin
+  :: (Reflex t, Monad m, MonadQuery t (Vessel V (Const SelectedCount)) m)
+  => Dynamic t (Maybe (Key Login Identity))
+  -> m (Dynamic t (Maybe (Login Identity)))
+queryLogin dmk = getResult <$> queryDyn query
+  where
+    query = ffor dmk $ \case
+      Just k -> singletonV V_Login $ MapV $ MM.singleton k $ Const 1
+      Nothing -> mempty
+    getResult dr = ffor2 dr dmk $ \r -> \case
+      Just k -> lookupV V_Login r >>= MM.lookup k . unMapV >>= getFirst . runIdentity
+      Nothing -> Nothing
+
+queryLogins
+  :: (Reflex t, Monad m, MonadQuery t (Vessel V (Const SelectedCount)) m)
+  => m (Dynamic t (Maybe (Set (Key Login Identity))))
+queryLogins = fmap getResult <$> queryDyn query
+  where
+    query = pure $ singletonV V_Logins $ SingleV $ Const 1
+    getResult = lookupV V_Logins >=> (getFirst . runIdentity . unSingleV)
